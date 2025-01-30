8321750f02b025b639b9029903304e56368e5bc1
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-01-30 13:07:26 +0100

    remove some files

--- apps/www/app/routes/_public/showcase/deployer.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_public/showcase/deployer")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/_public/showcase/deployer"!</div>;
-}
