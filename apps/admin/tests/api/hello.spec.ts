426726ba0ce1d3c9fea3ed66d44f31c3b15b12a6
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-02-01 13:27:37 +0100

    remove nez-admin & admin

--- apps/admin/tests/api/hello.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("/api/hello", () => {
-  it("should return a message", async () => {
-    const response = await fetch("http://localhost:3000/api/hello");
-    expect(response.status).toBe(200);
-    expect(await response.json()).toEqual({ message: 'Hello "/api/hello"!' });
-  });
-});
