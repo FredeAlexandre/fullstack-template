e388df4472eaf0786d3a5d0cd2c8d2c05c5c64cd
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-02-01 20:28:55 +0100

    add shadcn admin

---
+++ apps/admin/app.config.ts
@@ -0,0 +1,20 @@
+// app.config.ts
+import { defineConfig } from '@tanstack/start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        // fix loading all icon chunks in dev mode
+        // https://github.com/tabler/tabler-icons/issues/1233
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      },
+    },
+  },
+})
---
+++ apps/admin/app/routes/__root.tsx
@@ -0,0 +1,76 @@
+import { ReactNode } from 'react'
+import { QueryClient } from '@tanstack/react-query'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import appCss from '@/styles/app.css?url'
+import { Meta, Scripts } from '@tanstack/start'
+import { FontProvider } from '@/context/font-context'
+import { ThemeProvider } from '@/context/theme-context'
+import { Toaster } from '@/components/ui/toaster'
+import GeneralError from '@/features/errors/general-error'
+import NotFoundError from '@/features/errors/not-found-error'
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: "Admin dashboard"
+      },
+      { name: "description", content: "The admin dashboard of acme" },
+    ],
+    links: [
+      { rel: 'stylesheet', href: appCss },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon-96x96.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+      { rel: 'icon', href: '/favicon.ico' },
+    ],
+  }),
+  component: RootComponent,
+  notFoundComponent: NotFoundError,
+  errorComponent: GeneralError,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+        <FontProvider>
+          <Outlet />
+        </FontProvider>
+      </ThemeProvider>
+      <Toaster />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <Meta />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
