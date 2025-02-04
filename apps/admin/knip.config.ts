1dce437ec0987f7bf000b655e78e93e065ecc09f
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-02-04 21:29:10 +0100

    add knip

--- apps/admin/knip.config.ts
+++
@@ -1,9 +0,0 @@
-import type { KnipConfig } from 'knip';
-
-const config: KnipConfig = {
-  entry: ["app/ssr.tsx", "app/routeTree.gen.ts", "app/router.tsx", "app/client.tsx", "app.config.ts"],
-  project: ["app/**/**.ts", "app/**/*.tsx"],
-  ignore: ['app/components/ui/**', 'app/routeTree.gen.ts']
-};
-
-export default config;
