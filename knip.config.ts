3440670d7ad8810f1ec25b2532279ae749bb65e9
Alexandre Frede <alexandre.frede.pro@gmail.com>
2025-02-20 22:49:40 +0100

    bun add sahdcn ui all components

--- knip.config.ts
+++ knip.config.ts
@@ -3,8 +3,11 @@ import type { KnipConfig } from "knip"
 export default {
   workspaces: {
     "apps/*": {
-      "entry": ["app/*.ts", "app/*.tsx", "app.config.ts"],
+      "entry": ["app/*.ts", "app/*.tsx", "app.config.ts", "eslint.config.js"],
       ignoreDependencies: ["postcss", "tailwindcss", "tailwindcss-animate"]
+    },
+    "packages/ui": {
+      "entry": ["src/*.ts", "src/*.tsx", "src/hooks/*.ts", "src/hooks/*.tsx", "eslint.config.js"],
     }
   }
 } satisfies KnipConfig
