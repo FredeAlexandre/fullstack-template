ba41244d38aa29036790b5f8894e8e89797a704b
Alexandre Frede <alexandre.frede.pro@gmail.com>
2025-03-05 20:55:42 +0100

    update shadcn ui

--- packages/ui/src/sonner.tsx
+++ packages/ui/src/sonner.tsx
@@ -1,9 +1,8 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import * as React from "react";
-import { Toaster as Sonner } from "sonner";
-import { useTheme } from "./theme";
+"use client";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>;
+import { useTheme } from "next-themes";
+// biome-ignore lint/style/useImportType: <explanation>
+import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
 	const { theme = "system" } = useTheme();
@@ -18,9 +17,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
 						"group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
 					description: "group-[.toast]:text-muted-foreground",
 					actionButton:
-						"group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+						"group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium",
 					cancelButton:
-						"group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+						"group-[.toast]:bg-muted group-[.toast]:text-muted-foreground font-medium",
 				},
 			}}
 			{...props}
--- packages/ui/src/toaster.tsx
+++ packages/ui/src/toaster.tsx
@@ -13,16 +13,21 @@ export function Toaster() {
 
 	return (
 		<ToastProvider>
-			{toasts.map(({ id, title, description, action, ...props }) => (
-				<Toast key={id} {...props}>
-					<div className="grid gap-1">
-						{title && <ToastTitle>{title}</ToastTitle>}
-						{description && <ToastDescription>{description}</ToastDescription>}
-					</div>
-					{action}
-					<ToastClose />
-				</Toast>
-			))}
+			{
+				// @ts-ignore
+				toasts.map(({ id, title, description, action, ...props }) => (
+					<Toast key={id} {...props}>
+						<div className="grid gap-1">
+							{title && <ToastTitle>{title}</ToastTitle>}
+							{description && (
+								<ToastDescription>{description}</ToastDescription>
+							)}
+						</div>
+						{action}
+						<ToastClose />
+					</Toast>
+				))
+			}
 			<ToastViewport />
 		</ToastProvider>
 	);
